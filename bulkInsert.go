ab79dc16f2692372fa4a7780337b9fe35b9b0157
Maksim Falaleev <maxfalaleev1@gmail.com>
2024-05-23 16:41:25 +0300

    fix regex

--- bulkInsert.go
+++ bulkInsert.go
@@ -8,7 +8,7 @@ import (
 )
 
 var paramsRegex = regexp.MustCompile(`(\$(\d+))`)
-var valuesRegex = regexp.MustCompile(`(?ms)VALUES \((.*)\)$`)
+var valuesRegex = regexp.MustCompile(`(?ms)VALUES \((.*?)\)$`)
 
 func FindColumns(sql string) []string {
 	return paramsRegex.FindAllString(sql, -1)
--- bulkInsert_test.go
+++ bulkInsert_test.go
@@ -28,26 +28,59 @@ func TestBulkInsert(t *testing.T) {
 		}
 	})
 
-	t.Run("replace", func(t *testing.T) {
+	t.Run("replace_singleline", func(t *testing.T) {
 		t.Parallel()
 
-		sql := sqlcbulk.ReplaceValues(`VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9,
-        $10, $11, $12, $13)
-ON CONFLICT (code) DO UPDATE SET`, `($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`)
-		require.Equal(t, `VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
-ON CONFLICT (code) DO UPDATE SET`, sql)
+		sql := sqlcbulk.ReplaceValues(singleline, `($1, $2, $3, $4), ($5, $6, $7, $8), ($9, $10, $11, $12)`)
+		require.Equal(t, `-- name: CreateCity :batchexec
+INSERT INTO cdek_city (code, city, fias_guid, city_uuid)
+VALUES ($1, $2, $3, $4), ($5, $6, $7, $8), ($9, $10, $11, $12)
+ON CONFLICT (code) DO UPDATE SET code = EXCLUDED.code`, sql)
+	})
+
+	t.Run("replace_multiline", func(t *testing.T) {
+		t.Parallel()
+
+		sql := sqlcbulk.ReplaceValues(multiline, `($1, $2, $3, $4), ($5, $6, $7, $8), ($9, $10, $11, $12)`)
+		require.Equal(t, `-- name: CreateCity :batchexec
+INSERT INTO cdek_city (code, city, fias_guid, city_uuid)
+VALUES ($1, $2, $3, $4), ($5, $6, $7, $8), ($9, $10, $11, $12)
+ON CONFLICT (code) DO UPDATE SET code = EXCLUDED.code`, sql)
+	})
+
+	t.Run("replace_multiline_where", func(t *testing.T) {
+		t.Parallel()
+
+		sql := sqlcbulk.ReplaceValues(where, `($1, $2, $3, $4, $5), ($6, $7, $8, $9, $10)`)
+		require.Equal(t, `-- name: CreateAddresses :batchexec
+INSERT INTO foobar AS ra (id, total, amount, version, created_at)
+VALUES ($1, $2, $3, $4, $5), ($6, $7, $8, $9, $10)
+ON CONFLICT (id, version) DO UPDATE SET created_at = EXCLUDED.created_at,
+                                        total      = EXCLUDED.total,
+                                        amount     = EXCLUDED.amount
+WHERE ra.created_at < EXCLUDED.created_at
+  AND (ra.total != EXCLUDED.total OR ra.amount != EXCLUDED.amount)
+RETURNING id, total, amount, version, created_at`, sql)
 	})
 }
+
+const where = `-- name: CreateAddresses :batchexec
+INSERT INTO foobar AS ra (id, total, amount, version, created_at)
+VALUES ($1, $2, $3, $4, $5)
+ON CONFLICT (id, version) DO UPDATE SET created_at = EXCLUDED.created_at,
+                                        total      = EXCLUDED.total,
+                                        amount     = EXCLUDED.amount
+WHERE ra.created_at < EXCLUDED.created_at
+  AND (ra.total != EXCLUDED.total OR ra.amount != EXCLUDED.amount)
+RETURNING id, total, amount, version, created_at`
 
 const multiline = `-- name: CreateCity :batchexec
 INSERT INTO cdek_city (code, city, fias_guid, city_uuid)
 VALUES ($1, $2, 
 $3, $4)
-ON CONFLICT (code) DO UPDATE SET code = EXCLUDED.code;
-`
+ON CONFLICT (code) DO UPDATE SET code = EXCLUDED.code`
 
 const singleline = `-- name: CreateCity :batchexec
 INSERT INTO cdek_city (code, city, fias_guid, city_uuid)
 VALUES ($1, $2, $3, $4)
-ON CONFLICT (code) DO UPDATE SET code = EXCLUDED.code;
-`
+ON CONFLICT (code) DO UPDATE SET code = EXCLUDED.code`
